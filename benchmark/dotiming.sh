7176d4b18fd70ad7eba914d7eb89cd66df79e90d
rns <ruslan.shvedov@gmail.com>
2015-01-20 19:00:17 +0200

    work on cygwin benchmark

--- benchmark/dotiming.sh
+++ benchmark/dotiming.sh
@@ -1,12 +1,11 @@
 time=/usr/bin/time
-LUA_PATH="../lua/?.lua;${LUA_PATH}"
 
 echo Date: `date` >timings.out
 echo Platform: `uname -a` >> timings.out
 echo "============================================" >> timings.out
 echo "DESCRIPTION     SYST  USER  REAL    MAXRES  " >> timings.out
 echo "-----------     ----  ----  ----  ----------" >> timings.out
-for i in 0 #1 2
+for i in 0
 do
 
 $time -o timings.out --append --format="c libmarpa      %S  %U  %e  %M" ./a.out test.in
@@ -19,6 +18,8 @@ $time -o timings.out --append --format="lua dkjson      %S  %U  %e  %M" lua -e '
 
 $time -o timings.out --append --format="python json     %S  %U  %e  %M" python -c "import sys,json;from pprint import pprint;pprint(json.loads(sys.stdin.read()))" < test.in
 $time -o timings.out --append --format="python libmarpa %S  %U  %e  %M" python ../python/json-libmarpa.py test.in
+
+:$time -o timings.out --append --format="luajit libmarpa %S  %U  %e  %M" luajit ../lua/json-libmarpa.lua test.in
 
 done >/dev/null 2>&1
 
