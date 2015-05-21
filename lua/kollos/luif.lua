4c2d75e852bd7500bd1201d3a19371302f9c7d79
rns <ruslan.shvedov@gmail.com>
2015-05-21 17:43:26 +0300

    D2L: loop over rules via iterator

--- lua/kollos/luif.lua
+++ lua/kollos/luif.lua
@@ -42,8 +42,23 @@ local function xrule_type(rhs)
   end
 end
 
--- possible todo: iterators over LUIF grammar rules, RHS alternatives and symbols
+-- todo: iterators for RHS alternatives and symbols
 
+-- grammar rules iterator based on next()
+-- returns type of xrule after lhs and rhs
+local function rule_next(grammar, lhs)
+  local rhs
+  lhs, rhs = next(grammar, lhs)
+  if lhs == nil then return nil else
+    return lhs, rhs, xrule_type(rhs)
+  end
+end
+
+local function rules(grammar)
+  return rule_next, grammar, nil
+end
+
+-- produce xrule and xsym databases for the KIR g1 and l0 grammars
 function luif.G (grammar)
   assert( type(grammar) == "table", "grammar must be a table" )
   -- get grammar location
@@ -52,8 +67,7 @@ function luif.G (grammar)
   local g1 = { xrule = {}, xsym = {}, structural = true }
   local l0 = { xrule = {}, xsym = {} }
   -- iterate over rules (D2L tables)
-  for lhs, rhs in pairs(grammar) do
-    local xrule_type = xrule_type (rhs)
+  for lhs, rhs, xrule_type in rules(grammar) do
     -- wrap single-alternative RHS's and counted rules for
     -- the iteration over RHS alternatives below
     if xrule_type == 'BNF' or xrule_type == 'counted' then
