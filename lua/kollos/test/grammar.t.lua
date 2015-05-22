004463075c974cb4202975f698e27538224f7a24
rns <ruslan.shvedov@gmail.com>
2015-05-22 15:25:51 +0300

    THIF for LUIF prototyping: unseparated sequence: t+

--- lua/kollos/test/grammar.t.lua
+++ lua/kollos/test/grammar.t.lua
@@ -8,13 +8,19 @@ local grammar = require 'grammar'
 local g = grammar.new()
 
 local S1 = g:symbol_new('S1')
-ok ( S1 >= 0, 'new symbol' )
+ok ( S1 >= 0, 'symbol' )
 
 local S2 = g:symbol_new('S2')
-ok ( S2 >= 0, 'new symbol' )
+ok ( S2 >= 0, 'symbol' )
 
-local R = g:rule_new(S1, { S2 })
-ok ( R >= 0, 'new rule' )
+local R = g:rule_new( S1, { S2 } )
+ok ( R >= 0, 'rule' )
+
+local S3 = g:symbol_new('S3')
+ok ( S3 >= 0, 'symbol' )
+
+local S = g:sequence_new(S3, S2, -1, '+', 2)
+ok ( S >= 0, 'unseparated sequence rule' )
 
 g:start_symbol_set(S1)
 ok ( g:start_symbol() >= 0, "start symbol set" )
